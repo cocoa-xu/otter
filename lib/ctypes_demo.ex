2216836a498e298464d26918a54515a5fb3b6de7
Cocoa <r.xu.1@research.gla.ac.uk>
2022-02-11 14:07:43 +0000

    updated demo code

--- lib/ctypes_demo.ex
+++ lib/ctypes_demo.ex
@@ -17,16 +17,16 @@ defmodule Ctypes do
                 {:win32, _} -> raise "Windows is not supported yet"
               end)
   @load_mode :RTLD_NOW
-  extern(sin(:f64, f64))
+  extern sin(:f64, f64)
 
   # or using module level default shared library name and load mode
-  extern(puts(:s32, c_ptr))
-  extern(printf(:s32, c_ptr))
-  extern(printf(:s32, c_ptr, va_args))
-  extern(dlopen(:c_ptr, c_ptr, s32))
-  extern(dlsym(:c_ptr, c_ptr, c_ptr))
+  extern puts(:s32, c_ptr)
+  extern printf(:s32, c_ptr)
+  extern printf(:s32, c_ptr, va_args)
+  extern dlopen(:c_ptr, c_ptr, s32)
+  extern dlsym(:c_ptr, c_ptr, c_ptr)
 
   # explict mark argument name and type
-  extern(cos(:f64, theta :: f64))
-  extern(tan(:f64, theta :: f64))
+  extern cos(:f64, theta :: f64)
+  extern tan(:f64, theta :: f64)
 end
