263325c036c1412fe414f428b9cc0ebdc2c17a64
Cocoa <r.xu.1@research.gla.ac.uk>
2022-02-12 16:15:27 +0000

    minimum required elixir ver 1.12?

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Otter.MixProject do
     [
       app: :otter,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       compilers: [:elixir_make] ++ Mix.compilers(),
       description: description(),
